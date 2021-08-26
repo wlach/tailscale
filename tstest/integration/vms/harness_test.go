991add62cb78781adec40d27f8d5b2d1dd5aa3c5
David Crawshaw <crawshaw@tailscale.com>
2021-08-26 15:03:27 -0400

    vms: make ssh-keygen quiet by default
    Always succeeds. Its output clutters the log.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- tstest/integration/vms/harness_test.go
+++ tstest/integration/vms/harness_test.go
@@ -96,7 +96,12 @@ func newHarness(t *testing.T) *Harness {
 	hs := &http.Server{Handler: mux}
 	go hs.Serve(ln)
 
-	run(t, dir, "ssh-keygen", "-t", "ed25519", "-f", "machinekey", "-N", ``)
+	cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-f", "machinekey", "-N", ``)
+	cmd.Dir = dir
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Logf("ssh-keygen: %s", out)
+		t.Fatalf("ssh-keygen: %v", err)
+	}
 	pubkey, err := os.ReadFile(filepath.Join(dir, "machinekey.pub"))
 	if err != nil {
 		t.Fatalf("can't read ssh key: %v", err)
